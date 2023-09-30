12245b119a9ed737c6639dbcccc20d2ef577ed8e
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2023-09-30 01:07:51 +0100

    Fixed deployment error

--- src/pages/admin/Admin.tsx
+++ src/pages/admin/Admin.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import styles from "./Admin.module.scss";
-import Sidebar from "../../components/admin/sidebar/Sidebar";
-import { Route, Routes } from "react-router-dom";
-import Home from "../../components/admin/home/Home";
-import Profile from "../../components/admin/profile/Profile";
-import Sub from "../../components/admin/subscription/Sub";
+import styles from './Admin.module.scss';
+import Sidebar from '../../components/admin/sidebar/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import Home from '../../components/admin/home/Home';
+import Profile from '../../components/admin/profile/Profile';
+import Sub from '../../components/admin/subscription/Sub';
 
 const Admin = () => {
   return (
